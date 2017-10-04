b8f463acd0e58df335b83a4c0c0bc185069d1eaa
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2017-10-04 13:26:06 -0300

    Add <Sorted Union.js>

---
+++ freeCodeCamp - Algorithms/Intermediate/Sorted Union.js
@@ -0,0 +1,52 @@
+/*   Sorted Union.js
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+Check the assertion tests for examples.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Arguments object
+Array.prototype.reduce()
+Run tests (ctrl + enter)
+Reset your code
+Get a hint
+Ask for help on the forum
+Sign in so you can save your progress
+
+  * Your output will go here.
+  * Any console.log() -type
+  * statements will appear in
+  * your browser's DevTools
+  * JavaScript console as well.
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) should return [1, 3, 2, [5], [4]].
+uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
+
+*/
+
+
+function uniteUnique(arr) {
+  var newArr = [];
+  var args = Array.from(arguments); 
+  var count = 0;
+
+  for(var i=0; i<args.length; i++){
+    for(var j=0; j<args[i].length; j++){
+      if(newArr.indexOf(args[i][j]) === -1){
+        newArr.push(args[i][j]);
+      }
+    }
+  }
+  
+  return newArr;
+}
+
+uniteUnique([1, 3, 2], [1, [5]], [2, [4]]);
