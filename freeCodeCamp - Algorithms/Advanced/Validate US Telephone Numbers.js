e83a361c30b56a235a71d9b3713050ac5067c2df
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2019-07-08 10:04:27 -0300

    format

--- freeCodeCamp - Algorithms/Advanced/Validate US Telephone Numbers.js
+++ freeCodeCamp - Algorithms/Advanced/Validate US Telephone Numbers.js
@@ -1,8 +1,8 @@
 /* Validate US Telephone Numbers
 
 Return true if the passed string is a valid US phone number.
-
-The user may fill out the form field any way they choose as long as it is a valid US number. The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+The user may fill out the form field any way they choose as long as it is a valid US number.
+The following are examples of valid formats for US numbers (refer to the tests below for other variants):
 
 555-555-5555
 (555)555-5555
@@ -10,24 +10,17 @@ The user may fill out the form field any way they choose as long as it is a vali
 555 555 5555
 5555555555
 1 555 555 5555
-For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.
 
-Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
-
-Here are some helpful links:
-
-RegExp
-
+For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf.
+Your job is to validate or reject the US phone number based on any combination of the formats provided above.
+The area code is required. If the country code is provided, you must confirm that the country code is 1.
+Return true if the string is a valid US phone number; otherwise return false.
 */
 
 function telephoneCheck(str) {
-  // Good luck!
+  const regExp = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?(\d{3})[\s\-]?(\d{4})$/;
   
-  var regExp = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?(\d{3})[\s\-]?(\d{4})$/;
-  
-  
-  // (1\s?)  means that num begins with 1 and an optional space
-  //note that the first element is also optional
+  // (1\s?)  means that num begins with 1 and an optional space. Note that the first element is also optional.
 
   //second element: 
     // \(\d{3}\) means "(" + 3 digits + ")"
@@ -37,11 +30,7 @@ function telephoneCheck(str) {
   //third element idem second. 
   //idem optional char. 
   //last element: four digits. 
-  
-  //important!! http://regexr.com/
-
   return regExp.test(str);
-  
 }
 
 telephoneCheck("1 456 789 4444");
