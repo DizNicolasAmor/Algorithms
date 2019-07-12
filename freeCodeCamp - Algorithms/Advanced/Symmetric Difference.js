e051a25a6a603071f15817ce94bca9cf7f771b61
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2019-07-12 15:30:50 -0300

    refactor

--- freeCodeCamp - Algorithms/Advanced/Symmetric Difference.js
+++ freeCodeCamp - Algorithms/Advanced/Symmetric Difference.js
@@ -1,40 +1,27 @@
 /*  Symmetric Difference.js
 
-Create a function that takes two or more arrays and returns an array of the symmetric difference (△ or ⊕) of the provided arrays.
+Create a function that takes two or more arrays and returns an array of the symmetric difference.
 
-Given two sets (for example set A = {1, 2, 3} and set B = {2, 3, 4}), the mathematical term "symmetric difference" of two sets is the set of elements which are in either of the two sets, but not in both (A △ B = C = {1, 4}). For every additional symmetric difference you take (say on a set D = {2, 3}), you should get the set with elements which are in either of the two the sets but not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}).
+Given two sets (for example set A = {1, 2, 3} and set B = {2, 3, 4}), the mathematical term "symmetric difference"
+of two sets is the set of elements which are in either of the two sets, but not in both (A △ B = C = {1, 4}).
 
-Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
-
-Here are some helpful links:
-
-Array.prototype.reduce()
-Symmetric Difference
-
+For every additional symmetric difference you take (say on a set D = {2, 3}), you should get the set with elements
+which are in either of the two the sets but not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}).
 */
 
+const sym = (...args) => {
+	const result = [];
+	const argsCopy = args.map(arr => ([...arr]));
 
-function sym(args) {
-  //call all arguments
-  args = Array.from(arguments);
-  
+	argsCopy.forEach((currentArray, index) => {
+		const otherArrays = argsCopy.filter((ar, ix) => ix !== index);
+		const isInOtherArray = elem => otherArrays.some(arr => arr.includes(elem));
 
-  function diffArray(arr1, arr2){
-    var filter = [];
-    for(var i=0; i<arr1.length; i++){
-      if(arr2.indexOf(arr1[i]) === -1 && filter.indexOf(arr1[i]) === -1){
-        filter.push(arr1[i]);
-      }  
-    }
-    for(var j=0; j<arr2.length; j++){
-      if(arr1.indexOf(arr2[j]) === -1 && filter.indexOf(arr2[j]) === -1){
-        filter.push(arr2[j]);
-      }  
-    }
-    return filter;
-  } //diffArray
-  
-  return args.reduce(diffArray);
-}
+    currentArray.forEach(num => { if (!isInOtherArray(num)) result.push(num); });
+	});
 
-sym([1, 2, 3], [5, 2, 1, 4]);
+	return result;
+};
+
+console.log(sym([1, 2, 3], [5, 2, 1, 4])); /* [3, 5, 4] */
+console.log(sym([1, 2, 3], [5, 2, 1, 4], [5, 6, 9], [8, 9])); /* [3, 4, 6, 8] */
