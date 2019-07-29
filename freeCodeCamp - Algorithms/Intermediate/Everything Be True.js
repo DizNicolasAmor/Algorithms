92d6b22035318991b4fcd30fe36c3bbd1ac83aac
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2019-07-29 15:26:02 -0300

    refactor

--- freeCodeCamp - Algorithms/Intermediate/Everything Be True.js
+++ freeCodeCamp - Algorithms/Intermediate/Everything Be True.js
@@ -1,21 +1,7 @@
-/*  Everything Be True.js
-Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+/*
+Everything Be True.js
 
-Remember, you can access object properties through either dot notation or [] notation.
-
-Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
-
-Run tests (ctrl + enter)
-Reset your code
-Get a hint
-Ask for help on the forum
-Sign in so you can save your progress
-
-  * Your output will go here.
-  * Any console.log() -type
-  * statements will appear in
-  * your browser's DevTools
-  * JavaScript console as well.
+Check if the predicate (second argument) is truthy on all elements of a collection (first argument). Test cases:
 
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return true.
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return false.
@@ -28,16 +14,6 @@ truthCheck([{"single": "double"}, {"single": undefined}], "single") should retur
 truthCheck([{"single": "double"}, {"single": NaN}], "single") should return false
 */
 
-function truthCheck(collection, pre) {
-  // Is everyone being true?
-  
-  //every()  gives a boolean answer
-  //so, if the obj has not the prop  <pre>, it will be false. 
-  var answer = collection.every(function(obj){
-    return obj[pre];
-    }); 
-
-  return answer;
-}
+const truthCheck = (collection, pre) => collection.every(obj => obj[pre]);
 
 truthCheck([{"single": "yes"}], "single");
