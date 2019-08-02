dbde87b8e11bc5e215686c472d7843412e3ae4cf
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2019-08-02 11:02:18 -0300

    refactor

--- freeCodeCamp - Algorithms/Intermediate/Boo who.js
+++ freeCodeCamp - Algorithms/Intermediate/Boo who.js
@@ -1,48 +1,18 @@
-/*   Boo who.js
-Check if a value is classified as a boolean primitive. Return true or false.
+/*
 
-Boolean primitives are true and false.
+Boo who.js
 
-Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+Check if a value is classified as a boolean primitive. Return true or false. Test cases:
 
-Here are some helpful links:
-
-Boolean Objects
-Run tests (ctrl + enter)
-Reset your code
-Get a hint
-Ask for help on the forum
-Sign in so you can save your progress
-
-  * Your output will go here.
-  * Any console.log() -type
-  * statements will appear in
-  * your browser's DevTools
-  * JavaScript console as well.
-
-booWho(true) should return true.
-booWho(false) should return true.
-booWho([1, 2, 3]) should return false.
-booWho([].slice) should return false.
-booWho({ "a": 1 }) should return false.
-booWho(1) should return false.
-booWho(NaN) should return false.
-booWho("a") should return false.
-booWho("true") should return false.
-booWho("false") should return false.
-
+console.log(booWho(true)); // should return true.
+console.log(booWho(false)); // should return true.
+console.log(booWho([1, 2, 3])); // should return false.
+console.log(booWho([].slice)); // should return false.
+console.log(booWho({ "a": 1 })); // should return false.
+console.log(booWho(1)); // should return false.
+console.log(booWho(NaN)); // should return false.
+console.log(booWho("a")); // should return false.
+console.log(booWho("true")); // should return false.
+console.log(booWho("false")); // should return false.
 */
-
-
-function booWho(bool) {
-  // What is the new fad diet for ghost developers? The Boolean.
-  if(bool === true || bool === false){
-    return true;
-  }else{
-    return false;
-  }
-  
-  return bool;
-}
-
-booWho(null);
+const booWho = bool => typeof bool === 'boolean';
