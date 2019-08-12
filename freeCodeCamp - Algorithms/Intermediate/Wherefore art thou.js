a2d8dce86157f0d70f7a6ebdffef9a939beee751
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2019-08-12 10:26:58 -0300

    refactor

--- freeCodeCamp - Algorithms/Intermediate/Wherefore art thou.js
+++ freeCodeCamp - Algorithms/Intermediate/Wherefore art thou.js
@@ -1,62 +1,33 @@
-/*   Wherefore art thou.js
+/*
 
-Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching property and value pairs (second argument). Each property and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+Wherefore art thou.js
 
-For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the property and its value, that was passed on as the second argument.
+Make a function that looks through an array of objects (first argument)
+and returns an array of all objects that have matching property and value pairs (second argument).
+Each property and value pair of the source object has to be present in the object from the collection
+if it is to be included in the returned array.
 
-Remember to use Read-Search-Ask if you get stuck. Write your own code.
-
-Here are some helpful links:
-
-Global Object
-Object.prototype.hasOwnProperty()
-Object.keys()
-Run tests (ctrl + enter)
-Reset your code
-Get a hint
-Ask for help on the forum
-Sign in so you can save your progress
-
-  * Your output will go here.
-  * Any console.log() -type
-  * statements will appear in
-  * your browser's DevTools
-  * JavaScript console as well.
+Test cases:
 
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }) should return [{ first: "Tybalt", last: "Capulet" }].
 whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 }) should return [{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }].
 whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 }) should return [{ "a": 1, "b": 2 }, { "a": 1, "b": 2, "c": 2 }].
 whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 }) should return [{ "a": 1, "b": 2, "c": 2 }].
-
 */
 
-
-function whatIsInAName(collection, source) {
-  // What's in a name?
-  var arr = [];
-  // Only change code below this line
-
-  var prop = Object.keys(source);
-  var hasSameProp = false; 
+const whatIsInAName = (collection, source) => {
+  let arr = [];
+  const prop = Object.keys(source);
+  let hasSameProp = false; 
     
-  for(var i=0; i<collection.length; i++){
-
-    for(var j=0; j<prop.length; j++){
-      if(collection[i].hasOwnProperty(prop[j]) && collection[i][prop[j]] == source[prop[j]]){
+  for (let i=0; i<collection.length; i++){
+    for (let j=0; j<prop.length; j++){
+      if (collection[i].hasOwnProperty(prop[j]) && collection[i][prop[j]] == source[prop[j]]){
         hasSameProp = true;
-      }else{
-        hasSameProp = false;
-      }
+      }else hasSameProp = false;
     }
     
-    if(hasSameProp){
-      arr = arr.concat(collection[i]);
-    }
-
+    if (hasSameProp) arr = arr.concat(collection[i]);
   }
-                             
-  // Only change code above this line
   return arr;
-}
-
-whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 });
+};
