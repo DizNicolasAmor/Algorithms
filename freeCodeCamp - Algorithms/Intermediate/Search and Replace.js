3862e5dd44c0148b1087fd98d473d842a9740996
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2019-09-24 09:25:30 -0300

    refactor

--- freeCodeCamp - Algorithms/Intermediate/Search and Replace.js
+++ freeCodeCamp - Algorithms/Intermediate/Search and Replace.js
@@ -1,61 +1,30 @@
-/*   Search and Replace.js 
+/*
+
+Search and Replace.js 
 
 Perform a search and replace on the sentence using the arguments provided and return the new sentence.
 
-First argument is the sentence to perform the search and replace on.
+- First argument is the sentence to perform the search and replace on.
+- Second argument is the word that you will be replacing (before).
+- Third argument is what you will be replacing the second argument with (after).
 
-Second argument is the word that you will be replacing (before).
+NOTE: Preserve the case of the original word when you are replacing it: if you replace "Book" with "dog", it should be replaced as "Dog".
 
-Third argument is what you will be replacing the second argument with (after).
+Test cases:
 
-NOTE: Preserve the case of the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
-
-Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
-
-Here are some helpful links:
-
-Array.prototype.splice()
-String.prototype.replace()
-Array.prototype.join()
-Run tests (ctrl + enter)
-Reset your code
-Get a hint
-Ask for help on the forum
-Sign in so you can save your progress
-
-  * Your output will go here.
-  * Any console.log() -type
-  * statements will appear in
-  * your browser's DevTools
-  * JavaScript console as well.
-
-myReplace("Let us go to the store", "store", "mall") should return "Let us go to the mall".
-myReplace("He is Sleeping on the couch", "Sleeping", "sitting") should return "He is Sitting on the couch".
-myReplace("This has a spellngi error", "spellngi", "spelling") should return "This has a spelling error".
-myReplace("His name is Tom", "Tom", "john") should return "His name is John".
-myReplace("Let us get back to more Coding", "Coding", "algorithms") should return "Let us get back to more Algorithms".
-
+console.log(myReplace("Let us go to the store", "store", "mall")); // "Let us go to the mall".
+console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting")); // "He is Sitting on the couch".
+console.log(myReplace("This has a spellngi error", "spellngi", "spelling")); // "This has a spelling error".
+console.log(myReplace("His name is Tom", "Tom", "john")); // "His name is John".
+console.log(myReplace("Let us get back to more Coding", "Coding", "algorithms")); // "Let us get back to more Algorithms".
 */
 
+const myReplace = (str, before, after) => {
+    const isUpperCase = char => char === char.toUpperCase();
+    const auxAfter= after.split('');
+    if (isUpperCase(before[0])) auxAfter[0] = auxAfter[0].toUpperCase();
+    else auxAfter[0] = auxAfter[0].toLowerCase();
+    const newAfter = auxAfter.join('');
 
-function myReplace(str, before, after) {
-  
-  var newAfter = after;
-  
-  if (before[0] == before[0].toUpperCase()) {
-    
-    newAfter = "";
-    newAfter = after[0].toUpperCase();
-    
-    for(var i=1; i<after.length; i++){
-      newAfter += after[i];
-    }
-  }
-
-  str = str.replace(before, newAfter);
-  
-  
-  return str;
-}
-
-myReplace("He is Sleeping on the couch", "Sleeping", "sitting");
+    return str.replace(before, newAfter);
+};
