f2e06a6fb812a63b061cd6c2fb5e5e30011ffa2c
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2019-09-27 16:28:32 -0300

    refactor

--- freeCodeCamp - Algorithms/Intermediate/Sum All Primes.js
+++ freeCodeCamp - Algorithms/Intermediate/Sum All Primes.js
@@ -1,57 +1,17 @@
-/*   Sum All Primes.js
-Sum all the prime numbers up to and including the provided number.
+/* Sum all the prime numbers up to and including the provided number. The provided number may not be a prime. */
 
-A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
+const sumPrimes = num => {
+    let answer = 0;  
+    const isPrime = num => {
+        for(let i = 2; i < num; i++) { if (num % i === 0) return false; }
+        return num > 1;
+    };
 
-The provided number may not be a prime.
+    for(let i = 2; i <= num; i +=1) { if (isPrime(i)) answer += i; }
 
-Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+    return answer;
+};
 
-Here are some helpful links:
-
-For Loops
-Array.prototype.push()
-Run tests (ctrl + enter)
-Reset your code
-Get a hint
-Ask for help on the forum
-Sign in so you can save your progress
-
-  * Your output will go here.
-  * Any console.log() -type
-  * statements will appear in
-  * your browser's DevTools
-  * JavaScript console as well.
-
-sumPrimes(10) should return a number.
-sumPrimes(10) should return 17.
-sumPrimes(977) should return 73156.
-
-
-*/
-
-
-function sumPrimes(num) {
-  var answer = 0;
-  
-  function prime(x){
-    var isPrime = true; 
-    
-    for(var j=2; j<x; j++){
-      if(x % j === 0){
-        isPrime = false; 
-      }
-    }
-    return isPrime;
-  }
-
-  for(var i=2; i<=num; i++){
-    if(prime(i) === true){
-      answer += i;
-    }
-  }
-
-  return answer;
-}
-
-sumPrimes(10);
+console.log(typeof sumPrimes(10) === 'number');
+console.log(sumPrimes(10) === 17);
+console.log(sumPrimes(977) === 73156);
