d7f92281a5b25475828be0f0b9dca9d21a07e3b9
Diz, Nicolás Amor <diz.nicolasamor@gmail.com>
2019-10-02 12:08:48 -0300

    refactor

--- freeCodeCamp - Algorithms/Intermediate/Steamroller.js
+++ freeCodeCamp - Algorithms/Intermediate/Steamroller.js
@@ -1,44 +1,15 @@
-/* Steamroller.js
-Flatten a nested array. You must account for varying levels of nesting.
+/* Flatten a nested array. You must account for varying levels of nesting. */
 
-Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
-
-Here are some helpful links:
-
-Array.isArray()
-Run tests (ctrl + enter)
-Reset your code
-Get a hint
-Ask for help on the forum
-Sign in so you can save your progress
-
-  * Your output will go here.
-  * Any console.log() -type
-  * statements will appear in
-  * your browser's DevTools
-  * JavaScript console as well.
-
-steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"].
-steamrollArray([1, [2], [3, [[4]]]]) should return [1, 2, 3, 4].
-steamrollArray([1, [], [3, [[4]]]]) should return [1, 3, 4].
-steamrollArray([1, {}, [3, [[4]]]]) should return [1, {}, 3, 4].
-
-*/
-
-
-function steamrollArray(arr) {
-  // I'm a steamroller, baby
-  var newArr = [];
-  
-  for(var i=0; i<arr.length; i++){
-    if(Array.isArray(arr[i]) === false){
-      newArr.push(arr[i]);
-    }else{
-      newArr = newArr.concat(steamrollArray(arr[i]));
+const steamrollArray = arr => {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i += 1) {
+        if (Array.isArray(arr[i])) newArr = newArr.concat(steamrollArray(arr[i]));
+        else newArr.push(arr[i]);
     }
-  }
+    return newArr;
+};
 
-  return newArr;
-}
-
-steamrollArray([1, [2], [3, [[4]]]]);
+console.log(JSON.stringify(steamrollArray([[["a"]], [["b"]]])) === JSON.stringify(["a", "b"]));
+console.log(JSON.stringify(steamrollArray([1, [2], [3, [[4]]]])) === JSON.stringify([1, 2, 3, 4]));
+console.log(JSON.stringify(steamrollArray([1, [], [3, [[4]]]])) === JSON.stringify([1, 3, 4]));
+console.log(JSON.stringify(steamrollArray([1, {}, [3, [[4]]]])) === JSON.stringify([1, {}, 3, 4]));
